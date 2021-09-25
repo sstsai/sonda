5b14c32b8411705771a4279707f2a1f604f10027
Shawn Tsai <sstsai@gmail.com>
2021-09-25 00:24:52 -0700

    working tiff and tiff16

--- src/tiff.h
+++ src/tiff.h
@@ -5,20 +5,6 @@
 namespace tiff {
 inline constexpr auto header_size = 8;
 inline constexpr auto ifd_size    = 12;
-enum class types : uint16_t {
-    byte,
-    ascii,
-    short_,
-    long_,
-    rational,
-    sbyte,
-    undefined,
-    sshort,
-    slong,
-    srational,
-    float_,
-    double_,
-};
 enum class tags : uint16_t {
     image_width                = 256,
     image_length               = 257,
@@ -32,9 +18,79 @@ enum class tags : uint16_t {
     y_resolution               = 283,
     resolution_unit            = 296,
 };
+enum class types : uint16_t {
+    byte = 1,
+    ascii,
+    short_,
+    long_,
+    rational,
+    sbyte,
+    undefined,
+    sshort,
+    slong,
+    srational,
+    float_,
+    double_,
+};
 enum class compressions : uint16_t {
     none     = 1,
     ccitt_3  = 2,
     packbits = 32773,
 };
+enum class photometric_interpretations : uint16_t {
+    white_is_zero = 0,
+    black_is_zero = 1,
+};
+enum class resolution_units : uint16_t {
+    none       = 0,
+    inch       = 1,
+    centimeter = 2,
+};
+enum class byte_order : uint16_t {
+    little_endian = 0x4949,
+    big_endian    = 0x4d4d,
+};
+inline constexpr auto magic_number = uint16_t{42};
+inline constexpr auto header(std::byte *out, uint32_t offset) -> std::byte *
+{
+    return bytes::to(out, byte_order::little_endian, magic_number, offset);
+}
+struct ifd_entry {
+    tags     tag;
+    types    type;
+    uint32_t count;
+    uint32_t value_or_offset;
+};
+BOOST_DESCRIBE_STRUCT(ifd_entry, (), (tag, type, count, value_or_offset))
+template <std::same_as<ifd_entry>... Ts>
+inline constexpr auto ifd(std::byte *out, Ts &&...entries) -> std::byte *
+{
+    constexpr auto count = static_cast<uint16_t>(sizeof...(entries));
+    return bytes::to(out, count, std::forward<Ts>(entries)...);
+}
+inline constexpr auto grayscale_ifd(std::byte *out, uint16_t width,
+                                    uint16_t height, uint16_t bits,
+                                    uint32_t image_offset,
+                                    uint32_t xresolution_offset,
+                                    uint32_t yresolution_offset) -> std::byte *
+{
+    auto bytes = (bits + 7) / 8;
+    return ifd(
+        out, ifd_entry{tags::image_width, types::short_, 1, width},
+        ifd_entry{tags::image_length, types::short_, 1, height},
+        ifd_entry{tags::bits_per_sample, types::short_, 1, bits},
+        ifd_entry{tags::compression, types::short_, 1,
+                  static_cast<uint16_t>(compressions::none)},
+        ifd_entry{
+            tags::photometric_interpretation, types::short_, 1,
+            static_cast<uint16_t>(photometric_interpretations::black_is_zero)},
+        ifd_entry{tags::strip_offsets, types::short_, 1, image_offset},
+        ifd_entry{tags::rows_per_strip, types::short_, 1, height},
+        ifd_entry{tags::strip_byte_counts, types::long_, 1,
+                  static_cast<uint32_t>(width * height * bytes)},
+        ifd_entry{tags::x_resolution, types::rational, 1, xresolution_offset},
+        ifd_entry{tags::y_resolution, types::rational, 1, yresolution_offset},
+        ifd_entry{tags::resolution_unit, types::short_, 1,
+                  static_cast<uint16_t>(resolution_units::none)});
+}
 } // namespace tiff
--- test/main.cpp
+++ test/main.cpp
@@ -2,16 +2,17 @@
 #include "tiff.h"
 #include "endian.h"
 #include <cassert>
+#include <fstream>
 struct base {
     char c;
 };
 BOOST_DESCRIBE_STRUCT(base, (), (c))
 struct test : base {
-    int a;
+    int   a;
     float b;
 };
 BOOST_DESCRIBE_STRUCT(test, (base), (a, b))
-int main(int ac, char **av)
+auto test_serial()
 {
     std::array<std::byte, 2048> buffer;
     using namespace bytes;
@@ -19,7 +20,7 @@ int main(int ac, char **av)
     using namespace endian;
     to(buffer.data(), types::long_, tags::image_width, be_int16(5),
        test{{'0'}, 6, 1.2f});
-    assert(buffer[0] == std::byte{3});
+    assert(buffer[0] == std::byte{4});
     assert(buffer[1] == std::byte{0});
     assert(buffer[2] == std::byte{0});
     assert(buffer[3] == std::byte{1});
@@ -30,15 +31,55 @@ int main(int ac, char **av)
     assert(buffer[8] == std::byte{0});
     assert(buffer[9] == std::byte{0});
     assert(buffer[10] == std::byte{0});
-    types l;
-    tags  i;
+    types    l;
+    tags     i;
     be_int16 b;
     test     t;
-    auto ptr = from(buffer.data(), l, i, b, t);
+    auto     ptr = from(buffer.data(), l, i, b, t);
     assert(l == types::long_);
     assert(i == tags::image_width);
     assert(b.value == 5);
     assert(t.c == '0');
     assert(t.a == 6);
     assert(t.b == 1.2f);
+}
+auto test_tiff()
+{
+    std::array<std::byte, 2048>      buffer;
+    constexpr auto                   width  = uint32_t{256};
+    constexpr auto                   height = uint32_t{256};
+    std::array<char, width * height> image;
+    std::iota(image.begin(), image.end(), uint8_t{0});
+    auto ptr = tiff::header(buffer.data(), width * height + 8 + 16);
+    ptr      = bytes::to(ptr, width, uint32_t{1}, height, uint32_t{1});
+    auto ofs = std::ofstream("test.tif", std::ios_base::binary);
+    ofs.write(reinterpret_cast<char *>(buffer.data()), ptr - buffer.data());
+    ofs.write(image.data(), image.size());
+    ptr =
+        tiff::grayscale_ifd(buffer.data(), width, height, 8, 8 + 16, 8, 8 + 8);
+    ptr = bytes::to(ptr, uint32_t{0});
+    ofs.write(reinterpret_cast<char *>(buffer.data()), ptr - buffer.data());
+}
+auto test_tiff16()
+{
+    std::array<std::byte, 2048>      buffer;
+    constexpr auto                   width  = uint32_t{256};
+    constexpr auto                   height = uint32_t{256};
+    std::array<char, width * height * 2> image;
+    std::iota(image.begin(), image.end(), uint8_t{0});
+    auto ptr = tiff::header(buffer.data(), width * height * 2 + 8 + 16);
+    ptr      = bytes::to(ptr, width, uint32_t{1}, height, uint32_t{1});
+    auto ofs = std::ofstream("test16.tif", std::ios_base::binary);
+    ofs.write(reinterpret_cast<char *>(buffer.data()), ptr - buffer.data());
+    ofs.write(image.data(), image.size());
+    ptr =
+        tiff::grayscale_ifd(buffer.data(), width, height, 16, 8 + 16, 8, 8 + 8);
+    ptr = bytes::to(ptr, uint32_t{0});
+    ofs.write(reinterpret_cast<char *>(buffer.data()), ptr - buffer.data());
+}
+int main(int ac, char **av)
+{
+    test_serial();
+    test_tiff();
+    test_tiff16();
 }
